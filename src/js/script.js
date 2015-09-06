ef8b28a57ac5e218580b2bc3ff8180f2eb0cf654
Anne-Sophie Pirlet <annesophie.pirlet@gmail.com>
2015-09-06 18:38:19 +0200

    essai js raté

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,17 @@
 
     $( function() {
         // Call my function here
-
+        // $('.chevaux__item+.chevaux__item').slideUp();
+        // var current=0;
+        // if($('.chevaux').length)
+        // $(window).one('scroll',function(){
+        //   console.log('lol');
+        //   $('.chevaux__item').eq(current).slideUp();
+        //   current++;
+        //   $('.chevaux__item').eq(current).slideDown();
+        //   $(window).off('scroll');
+        //
+        // });
     } );
 
 } ) (jQuery);
